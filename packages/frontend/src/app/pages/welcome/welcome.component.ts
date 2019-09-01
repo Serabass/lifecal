f5ffe082ea8b99323307d944b5b8da4b780dd3ea
Serabass <serabass565@gmail.com>
2019-09-01 17:37:33 +0600

    upd

--- packages/frontend/src/app/pages/welcome/welcome.component.ts
+++ packages/frontend/src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import * as moment from 'moment';
+
+export interface Point2D {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('canvas', {static: true})
+  public canvas: ElementRef<HTMLCanvasElement>;
 
-  ngOnInit() {
+  private ctx: CanvasRenderingContext2D;
+
+  @Input()
+  public width = 900;
+
+  @Input()
+  public height = 500;
+
+  public year: number;
+
+  public hoverPoint: Point2D = {
+    x: 0, y: 0
+  };
+
+  private yearOffset = 0;
+
+  constructor() {
   }
 
+  ngOnInit() {
+    this.ctx =  this.canvas.nativeElement.getContext('2d');
+    this.year = moment().year();
+
+    requestAnimationFrame(() => this.draw());
+  }
+
+  private draw() {
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(0, 0, this.width, this.height);
+
+    let year = this.year - 5 + this.yearOffset;
+
+    const offsetX = 70;
+    for (let x = 1; x <= 9; x++) {
+      for (let y = 1; y <= 9; y++) {
+        this.ctx.strokeStyle = 'black';
+
+        this.ctx.strokeRect(offsetX + x * 50, y * 50, 40, 40);
+      }
+    }
+
+    for (let y = 1; y <= 10; y++) {
+      this.ctx.strokeStyle = 'black';
+      this.ctx.font = '30px Arial';
+
+      this.ctx.strokeText((year + y).toString(), 0, y * 50 + 25 );
+    }
+  }
+
+  public mousemove(event: MouseEvent) {
+    this.hoverPoint.x = event.offsetX;
+    this.hoverPoint.y = event.offsetY;
+  }
 }
